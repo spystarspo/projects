05dea7aa7078bd0250b547f10af96d76614e5fa6
spy160 <109082271+spystarspo@users.noreply.github.com>
2022-08-10 03:24:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to Band_Name-Generator!\n")
+city=input("what is the name of city you love?\n")                #input to ask user
+pet=input("what is your pet name?\n")
+s=print("your band name could be \n"+ city + "_" + pet)
+print("your band_name size\n")
+print(len(city + " " + pet)  )
